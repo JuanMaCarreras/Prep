e223c173991d9ef31a253bdeebac337747a1b043
JuanMaCarrearas <manuucarreras@gmail.com>
2023-07-13 21:55:18 -0300

    06-JS-V

--- 06-JS-V/homework/homework.js
+++ 06-JS-V/homework/homework.js
@@ -43,6 +43,10 @@ function agregarStringInvertida() {
     // Ej: 'menem'.reverse() => menem
     // 'toni'.reverse() => 'inot'
     // Pista: Necesitarás usar "this" dentro de "reverse"
+
+    String.prototype.reverse = function () {
+        return this.split('').reverse().join('')
+    }
 }
 
 // ---------------------------------------------------------------------------//
@@ -57,10 +61,25 @@ function agregarStringInvertida() {
 //  }
 
 class Persona {
-    constructor(/*Escribir los argumentos que recibe el constructor*/) {
-        // Crea el constructor:
+    constructor(nombre, apellido, edad, domicilio) {
+        this.nombre = nombre
+        this.apellido = apellido
+        this.edad = edad
+        this.domicilio = domicilio
+    }
+
+    detalle() {
+        return {
+            Nombre: this.nombre,
+            Apellido: this.apellido,
+            Edad: this.edad,
+            Domicilio: this.domicilio,
+        }
     }
 }
+
+const persona = new Persona('Juan', 'Perez', 22, 'Saavedra 123')
+console.log(persona)
 
 function crearInstanciaPersona(nombre, apellido, edad, dir) {
     //Con esta función vamos a crear una nueva persona a partir de nuestro constructor de persona (creado en el ejercicio anterior)
