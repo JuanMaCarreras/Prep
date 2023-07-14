ae8618cf1af795f77e2a125b9d8db6011bcf5c8c
JuanMaCarrearas <manuucarreras@gmail.com>
2023-07-13 22:05:15 -0300

    06-JS-V Completo

--- 06-JS-V/homework/homework.js
+++ 06-JS-V/homework/homework.js
@@ -1,7 +1,7 @@
 // No cambies los nombres de las funciones.
 
 function crearUsuario() {
-    // Crea una Clase de ES6 o una función constructor llamada "Usuario"
+    // Crea una Clase de ES6 o una función constructor llamada "Usuario "
     // Debe aceptar un objeto "opciones" con las propiedades "usuario", "nombre", "email" y "password"
     // En el `contructor`, define el usuario, el nombre, el email y la contraseña
     // El `contructor` debe tener un método llamado "saludar" en su `prototype` que devuelva una string 'Hola, mi nombre es {{nombre}}'
