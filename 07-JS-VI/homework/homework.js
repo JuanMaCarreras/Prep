0b46373a52d14bc87a8632c0ba328953a80e5d74
JuanMaCarrearas <manuucarreras@gmail.com>
2023-07-16 22:17:09 -0300

    07-JS-VI Completo

--- 07-JS-VI/homework/homework.js
+++ 07-JS-VI/homework/homework.js
@@ -49,12 +49,17 @@ function map(array, cb) {
     // Itera sobre cada valor en "array", pásalo a `cb` y luego ubicar el valor devuelto por `cb` en un nuevo array
     // El nuevo array debe tener la misma longitud que el array del argumento
     //Tu código:
+    return array.map(cb)
 }
 
 function filter(array) {
     //Filtrar todos los elementos del array que comiencen con la letra "a".
     //Devolver un nuevo array con los elementos que cumplen la condición
     //Tu código:
+
+    return array.filter(
+        elemento => elemento.startsWith('a') || elemento.startsWith('A'),
+    )
 }
 
 // No modificar nada debajo de esta línea
